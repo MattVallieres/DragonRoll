60a657f338ac54884557e1b2fe5187a60c77b5bf
Matthew Vallieres <mvallieres@hotmail.ca>
2023-12-23 01:17:23 -0800

    quick auth0 setup

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,17 +1,21 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+// app/layout.jsx
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'DragonRoll',
-  description: 'Dungeons & Dragons character creation website',
-}
+  title: "DragonRoll",
+  description: "Dungeons & Dragons character creation website",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <UserProvider>
+        <body>{children}</body>
+      </UserProvider>
     </html>
-  )
+  );
 }
--- client/app/page.js
+++ client/app/page.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
+import ProfileClient from "./components/ProfileClient";
+
 export default function Home() {
   return (
     <main>
       <h1>Hello World!</h1>
+      <Link href="/api/auth/login">Login</Link>
+      <Link href="/api/auth/logout">Logout</Link>
+      <ProfileClient />
     </main>
   )
 }
